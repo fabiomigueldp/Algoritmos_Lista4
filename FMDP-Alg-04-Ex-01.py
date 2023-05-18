beb4640bd3b3082872cf0be0bd5542ae3a8d1ca4
fabiomigueldp <fabiomigueldp@gmail.com>
2023-05-18 05:12:38 -0300

    first commit

---
+++ FMDP-Alg-04-Ex-01.py
@@ -0,0 +1,6 @@
+'''1. Média aritmética. Escreva um programa Python que calcula a média aritmética de um
+conjunto de valores fornecidos pelo usuário. O usuário deve entrar com o valor 0 indicando
+que não serão mais fornecidos novos valores. Seu programa deve exibir uma mensagem de
+erro se o primeiro valor fornecido pelo usuário for 0.
+Dica: o número 0 não deve ser incluído no cálculo da média, pois ele só serve para sinalizar
+o final da entrada de dados.'''
