8b4a9327b273d7a3ddb2198e83853239c5adcfce
fabiomigueldp <fabiomigueldp@gmail.com>
2023-05-26 06:40:04 -0300

    commit 6

--- FMDP-Alg-04-Ex-10.py
+++ FMDP-Alg-04-Ex-10.py
@@ -2,3 +2,49 @@
 à string original. Por exemplo: “arara”, “osso”, “radar”. Escreva um programa Python usando
 laço de repetição para determinar se uma palavra fornecida pelo usuário é ou não é um
 palíndromo. Seu programa deve exibir uma mensagem informando o resultado.'''
+
+mensagem = input("Insira uma mensagem: ")
+
+"""
+Verifica se a mensagem é um palíndromo.
+Divide a mensagem em duas metades e verifica se a primeira metade é igual à segunda metade invertida.
+"""
+
+if len(mensagem) % 2 == 0:
+    metade1 = mensagem[0:(len(mensagem)//2)]
+    metade2 = mensagem[(len(mensagem)//2):]
+    imetade2 = metade2[::-1]
+
+    # Converte as letras da primeira metade em códigos Unicode
+    for letra in metade1:
+        codigo_metade1 = ord(letra)
+
+    # Converte as letras da segunda metade invertida em códigos Unicode
+    for letra in imetade2:
+        codigo_imetade2 = ord(letra)
+
+    # Verifica se os códigos Unicode das metades são iguais
+    if codigo_metade1 == codigo_imetade2:
+        print("Sua mensagem é um palíndromo.")
+    else:
+        print("Sua mensagem não é um palíndromo.")
+
+else:
+    metade1 = mensagem[0:(len(mensagem)//2)]
+    meio = mensagem[(len(mensagem)//2)]
+    metade2 = mensagem[(len(mensagem)//2)+1:]
+    imetade2 = metade2[::-1]
+
+    # Converte as letras da primeira metade em códigos Unicode
+    for letra in metade1:
+        codigo_metade1 = ord(letra)
+
+    # Converte as letras da segunda metade invertida em códigos Unicode
+    for letra in imetade2:
+        codigo_imetade2 = ord(letra)
+
+    # Verifica se os códigos Unicode das metades são iguais
+    if codigo_metade1 == codigo_imetade2:
+        print("Sua mensagem é um palíndromo.")
+    else:
+        print("Sua mensagem não é um palíndromo.")
--- FMDP-Alg-04-Ex-11.py
+++ FMDP-Alg-04-Ex-11.py
@@ -4,3 +4,53 @@ modifique o programa do exercício anterior para verificar se frases são palín
 programa vai precisar ignorar os espaços em brando das frases. Como desafio adicional,
 amplie sua solução para que também ignore sinais de pontuação e trate letras maiúsculas e
 minúsculas como equivalentes.'''
+
+mensagem = input("Insira uma mensagem: ")  # Solicita ao usuário que insira uma mensagem.
+mensagem = mensagem.lower()  # Converte a mensagem para minúsculas.
+mensagem = "".join(mensagem.split())  # Remove os espaços em branco da mensagem.
+mensagemx = ""
+
+for letra in mensagem:
+    if letra.isalpha():
+        mensagemx += letra
+
+mensagem = mensagemx  # Atribui a versão filtrada da mensagem à variável "mensagem".
+
+if len(mensagem) % 2 == 0:
+    metade1 = mensagem[0:(len(mensagem)//2)]
+    metade2 = mensagem[(len(mensagem)//2):]
+    imetade2 = metade2[::-1]
+
+    # Converte as letras da primeira metade em códigos Unicode
+    for letra in metade1:
+        codigo_metade1 = ord(letra)
+
+    # Converte as letras da segunda metade invertida em códigos Unicode
+    for letra in imetade2:
+        codigo_imetade2 = ord(letra)
+
+    # Verifica se os códigos Unicode das metades são iguais
+    if codigo_metade1 == codigo_imetade2:
+        print("Sua mensagem é um palíndromo.")
+    else:
+        print("Sua mensagem não é um palíndromo.")
+
+else:
+    metade1 = mensagem[0:(len(mensagem)//2)]
+    meio = mensagem[(len(mensagem)//2)]
+    metade2 = mensagem[(len(mensagem)//2)+1:]
+    imetade2 = metade2[::-1]
+
+    # Converte as letras da primeira metade em códigos Unicode
+    for letra in metade1:
+        codigo_metade1 = ord(letra)
+
+    # Converte as letras da segunda metade invertida em códigos Unicode
+    for letra in imetade2:
+        codigo_imetade2 = ord(letra)
+
+    # Verifica se os códigos Unicode das metades são iguais
+    if codigo_metade1 == codigo_imetade2:
+        print("Sua mensagem é um palíndromo.")
+    else:
+        print("Sua mensagem não é um palíndromo.")
